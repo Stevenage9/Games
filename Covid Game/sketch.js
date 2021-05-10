921549cb51cfdb5c321e74fee991b0e7633bb4b3
Stevenage9 <72202037+Stevenage9@users.noreply.github.com>
2021-05-10 17:53:51 +0530

    Add files via upload
    Updated, added collision and vaccine spawns on distance 2000

--- Covid Game/sketch.js
+++ Covid Game/sketch.js
@@ -3,6 +3,8 @@ var ground, groundImage
 var covid, covidimg
 var distance
 var bg
+var vaccine, vaccineImg
+var gameState
 
 function preload(){
   personImage=loadImage("assets/person.png")
@@ -12,7 +14,8 @@ function preload(){
 
   groundImage=loadImage("assets/ground.png")
 
-  
+  vaccineImg=loadImage("assets/vaccine.png")
+
   virus=loadImage("assets/virus.png")
   virus2=loadImage("assets/virus2.png")
 }
@@ -31,36 +34,79 @@ function setup(){
 
 covidGroup= new Group()
 
-  distance=0
+  distance=1800
+
+gameState=1
+
+  vaccine=createSprite(456, 860, 50,50)
+  vaccine.addImage("vaccine", vaccineImg)
+  vaccine.visible=false
 
 }
 
 function draw () {
   background(bg)
-  distance=distance+Math.round(getFrameRate()/60)
+
   fill("green")
   textSize(30)
   text("Distance travelled: "+ distance, 130, 100)
+if (gameState===1){
 
-if(keyDown("space")){
-  person.velocityY= -10
-}
+distance=distance+Math.round(getFrameRate()/60)
+
+if(keyDown("space")&&(person.y>856)){
+  person.velocityY= -17}
 
 if(ground.x<0){
-  ground.x=ground.width/3
+  ground.x=ground.width/3}
 
+if(distance>2000){
+  vaccine.visible=true
+  vaccine.velocityX=-2}
+
+
+if(person.collide(covidGroup)){
+    distance=distance-50
+    covidGroup.destroyEach
+    person.velocityX=0
+  }
+    
+if (person.collide(vaccine)){
+  gameState=2
 }
+
+
 
 spawnCovid()
 person.velocityY=person.velocityY+1
+
+}
+
+else if(gameState===2){
+vaccine.visible=false
+ground.velocityX=0
+person.velocityX=0
+person.velocityY=0
+
+fill("#fcca03")
+textSize(50)
+stroke("black")
+strokeWeight(5)
+text("You have been vaccinated and beat the virus", 0, 500)
+}
+
+
+
+
 person.collide(ground)
   drawSprites()
 }
 function spawnCovid() {
   if(frameCount % 120 === 0) {
-    var covid = createSprite(570,840,10,40);
-    covid.velocityX = -4;
+    var covid = createSprite(1050,900,10,40);
+    covid.velocityX = -10-(distance/100);
     
+
     //generate random obstacles
     var rand = Math.round(random(1,2));
     switch(rand) {
@@ -71,8 +117,8 @@ function spawnCovid() {
       default: break;
     }
     
-    //assign scale and lifetime to the obstacle           
-    covid.scale = 0.3;
+    //assign scale nad lifetime to the obstacle           
+    covid.scale = 0.2;
     covid.lifetime = 300;
     //add each obstacle to the group
     covidGroup.add(covid);
